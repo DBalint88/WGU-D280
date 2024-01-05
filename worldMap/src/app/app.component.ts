8b3cd9b9738e808885df773fb07697727830e5ea
DBalint88 <dbalint88@gmail.com>
2024-01-05 15:30:08 -0500

    Add SVG, world-map-component, grid layout w placeholder text

--- worldMap/src/app/app.component.ts
+++ worldMap/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { WorldMapComponent } from './world-map/world-map.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, WorldMapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
